8899427fbcd4669ef7c8c7d5a8fd6fd6bd56b020
John Khachian <jaksawesome10@gmail.com>
2022-12-24 10:29:26 -0500

    while loops

---
+++ whileLoops/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int index = 1;
+    while(index <= 5){
+        cout << index << endl;
+        index++;
+    }
+
+    cout << endl;
+
+    index = 1;
+    do{
+        cout << index << endl;
+        index++;
+    }while(index <= 5);
+
+    return 0;
+}
